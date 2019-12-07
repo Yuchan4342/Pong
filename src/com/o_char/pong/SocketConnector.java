62622ff7b52bc739dc6c1e2b5da8b7228768091e
Yuto Iwata <jyuchan415@gmail.com>
2019-12-07 11:26:41 +0900

    refactor SocketConnector.

--- src/com/o_char/pong/SocketConnector.java
+++ src/com/o_char/pong/SocketConnector.java
@@ -5,13 +5,21 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 
-// 外部からの接続(MAX_SOCKET_NUMBER 個以下のクライアントソケット)を受け付けるサーバーソケット
+/**
+ * 外部からの接続を受け付けるサーバーソケット.
+ */
 public final class SocketConnector implements Runnable {
   private PongServer ps;
-  private ServerSocket s;
-  private final int MAX_SOCKET_NUMBER;
+  private ServerSocket serverSocket;
+  private final int maxSocketNumber;
+  /**
+   * 最大のクライアントソケット数.
+   */
   private int socketNumber;
-  private final int TIME_OUT = 1000;
+  /**
+   * タイムアウト.
+   */
+  private final int timeOut = 1000;
 
   // 受信中かどうか
   private boolean isReceivedNow;
@@ -19,22 +27,33 @@ public final class SocketConnector implements Runnable {
   // 終了要求があったか
   private boolean isTermination;
 
-  private SocketConnector(PongServer nps, int n) {
+  private SocketConnector(PongServer nps, int number) {
     this.ps = nps;
-    this.MAX_SOCKET_NUMBER = n;
+    this.maxSocketNumber = number;
     this.socketNumber = 0;
   }
 
+  /**
+   * 新しい SocketConnector を生成して返す.
+   *
+   * @param ps         PongServer.
+   * @param portNumber ポート番号.
+   * @param n          最大のソケット数.
+   * @return 生成したソケット数.
+   * @throws IOException ServerSocket を開く時に I/O エラーが起こった場合.
+   */
   public static SocketConnector createConnector(PongServer ps, int portNumber, int n) throws IOException {
     SocketConnector sc = new SocketConnector(ps, n);
     sc.initialize(portNumber);
     return sc;
   }
 
+  /**
+   * 実行する. 外部からの接続を待機する.
+   */
   public void run() {
-
     // 終了要求がない間
-    while (this.isTermination() == false) {
+    while (!this.isTermination()) {
       // 新たな接続を受信する
       Socket socket = this.acceptConnection();
 
@@ -47,14 +66,14 @@ public final class SocketConnector implements Runnable {
       this.ps.acceptSocket(socket);
     }
 
-    // サーバーソケットsを閉じる
+    // サーバーソケットを閉じる.
     try {
-      System.out.println("Closing serversocket: " + this.s);
-      this.s.close();
+      System.out.println("Closing server socket: " + this.serverSocket);
+      this.serverSocket.close();
     } catch (IOException ioe) {
       // Do Nothing.
     } finally {
-      this.s = null;
+      this.serverSocket = null;
     }
   }
 
@@ -72,13 +91,17 @@ public final class SocketConnector implements Runnable {
     return this.socketNumber;
   }
 
+  /**
+   * socketNumber を増減させる.
+   * @param n 増減させる数. 1 または -1 が入る.
+   */
   public synchronized void transNumberOfSocket(int n) {
     if (n == -1) {
       this.socketNumber--;
-      if (this.socketNumber <= 0)
-        this.setReceivedNow(false);
-    } else if (n == 1)
+      this.setReceivedNow(this.socketNumber > 0);
+    } else if (n == 1) {
       this.socketNumber++;
+    }
   }
 
   public synchronized void terminate() {
@@ -89,28 +112,33 @@ public final class SocketConnector implements Runnable {
     return isTermination;
   }
 
-  // サーバーソケットの初期化
+  /**
+   * サーバーソケットを初期化する.
+   *
+   * @param portNumber 設定するポート番号.
+   * @throws IOException ServerSocket を開く時に I/O エラーが起こった場合.
+   */
   public void initialize(int portNumber) throws IOException {
-    this.s = new ServerSocket(portNumber);
-    this.s.setSoTimeout(this.TIME_OUT);
-    System.out.println("Started: " + this.s);
+    this.serverSocket = new ServerSocket(portNumber);
+    this.serverSocket.setSoTimeout(this.timeOut);
+    System.out.println("Started: " + this.serverSocket);
   }
 
   private Socket acceptConnection() {
-    Socket cSocket = null;
+    Socket connectingSocket = null;
 
     try {
-      cSocket = this.s.accept(); // コネクション設定要求を待つ
+      connectingSocket = this.serverSocket.accept(); // コネクション設定要求を待つ
     } catch (SocketTimeoutException toe) {
       // Do Nothing.
     } catch (IOException ioe) {
       System.err.println("ソケット受信に異常");
     }
 
-    if ((cSocket != null) && (this.getNumberOfSocket() >= this.MAX_SOCKET_NUMBER)) {
+    if ((connectingSocket != null) && (this.getNumberOfSocket() >= this.maxSocketNumber)) {
       System.err.println("Cannot connect: Connecting socket is Full.");
-      cSocket = null;
+      connectingSocket = null;
     }
-    return cSocket;
+    return connectingSocket;
   }
 }
